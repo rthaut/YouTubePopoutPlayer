93b8c600e554ff911ccd9c3501bb15a78e9dff84
rthaut <rthaut@gmail.com>
2017-10-27 15:36:02 -0600

    v2.1.2: fix missing browser polyfill script; declare additional logo icon sizes; add youtube-nocookie.com domain to userscript; revert to only building one localized userscript (due to update/download URLs); convert source to ES6 modules and use rollup for build process; generate PNG version of logo;

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,22 @@
 /* global require */
 const gulp = require('gulp');
+const rollup = require('rollup-stream');
+const buffer = require('vinyl-buffer');
+const source = require('vinyl-source-stream');
 
 const del = require('del');
 const fs = require('fs');
 const path = require('path');
-const merge = require('merge-stream');
 const sequence = require('run-sequence');
 
 const concat = require('gulp-concat');
 const ejs = require('gulp-ejs');
 const eslint = require('gulp-eslint');
+const folders = require('gulp-folders');
 const gulpIf = require('gulp-if');
 const header = require('gulp-header');
 const localize = require('gulp-browser-i18n-localize');
+const rename = require('gulp-rename');
 
 const composer = require('gulp-uglify/composer');
 const uglify = composer(require('uglify-es'), console);
@@ -24,14 +28,9 @@ const zip = require('gulp-zip');
 
 /* ==================== CONFIGURATION ==================== */
 
-// vendor libraries needed for core functionality
-const vendor = [
-    './node_modules/webextension-polyfill/dist/browser-polyfill.min.js'
-];
-
 // additional includes (custom libraries, classes, etc.)
 const includes = [
-
+    './node_modules/webextension-polyfill/dist/browser-polyfill.min.js',
 ];
 
 // load in package JSON as object for variables & EJS templates
@@ -39,12 +38,6 @@ const package = require('./package.json');
 
 // default options for various plugins
 const options = {
-    // options for compiling LESS to CSS
-    'less': {
-        'paths': 'node_modules',
-        'outputStyle': 'compressed',
-        'sourceMap': false
-    },
     // options for compressing JS files
     'uglify': {
         'compress': {
@@ -63,22 +56,45 @@ const _folders = {
 
 /* ====================  BUILD TASKS  ==================== */
 
-
-// tasks for cleaning the build directories
-gulp.task('clean:userscript', function () {
-    return del(['./dist/userscript/*'])
-        .catch(function (error) {
-            console.warn(error);
-        });
-});
-gulp.task('clean:webextension', function () {
-    return del(['./dist/webextension/*'])
+gulp.task('clean', function () {
+    return del(['./dist/*'])
         .catch(function (error) {
             console.warn(error);
         });
 });
 
-//task for linting the script files
+
+// ==========================================
+// build & lint tasks, broken into components
+// ==========================================
+
+gulp.task('build:includes', function () {
+    return gulp.src(includes)
+        .pipe(gulp.dest('./dist/webextension/scripts'));
+});
+
+
+gulp.task('build:locales', function () {
+    return gulp.src(path.join(_folders.locales, '/**/*.json'))
+        .pipe(gulp.dest('./dist/webextension/_locales'));
+});
+
+
+gulp.task('build:logo', function () {
+    return gulp.src(['./resources/logo.svg'])
+        .pipe(gulp.dest('./dist/webextension'));
+});
+
+
+gulp.task('build:manifest', function () {
+    return gulp.src(['./manifest.json'])
+        .pipe(ejs({
+            'package': package
+        }))
+        .pipe(gulp.dest('./dist/webextension'));
+});
+
+
 gulp.task('lint:scripts', function () {
     return gulp.src(path.join(_folders.scripts, '**/*.js'))
         .pipe(eslint({
@@ -86,72 +102,47 @@ gulp.task('lint:scripts', function () {
         }))
         .pipe(eslint.format());
 });
-
-// task for building the userscript version
-gulp.task('build:userscript', function () {
-    return gulp.src(path.join(_folders.scripts, '**/*.js'))
+gulp.task('build:scripts', ['lint:scripts'], folders(_folders.scripts, function (folder) {
+    return rollup({
+            'input': path.join(_folders.scripts, folder, 'index.js'),
+            'format': 'iife',
+            'name': package.title.replace(/\s/g, ''),
+        })
+        .pipe(source(folder + '.js'))
+        .pipe(buffer())
         .pipe(uglify(options.uglify))
-        .pipe(concat(package.name.replace(/\-/g, '_') + '.user.js'))
+        .pipe(header(fs.readFileSync('./banners/webextension.txt', 'utf8'), {
+            'package': package
+        }))
+        .pipe(gulp.dest('./dist/webextension/scripts'));
+}));
+
+// task for building a userscript
+gulp.task('build:userscript', ['build:scripts'], function () {
+    return gulp.src('./dist/webextension/scripts/content.js')
+        .pipe(rename(package.name.replace(/\-/g, '_')))
         .pipe(header(fs.readFileSync('./banners/userscript.txt', 'utf8'), {
             'package': package
         }))
         .pipe(localize({
-            'schema': '/$locale/$filename.$ext'
+            'locales': ['en'],
+            'schema': false
         }))
         .pipe(gulp.dest('./dist/userscript'));
 });
 
-// tasks for building the WebExtension version
-gulp.task('build:webextension:scripts', ['lint:scripts'], function () {
-    return merge(
-            gulp.src(includes)
-            .pipe(gulpIf('!*.min.js', uglify({
-                'mangle': false
-            }))),
-            gulp.src(path.join(_folders.scripts, '**/*.js'))
-            .pipe(concat(package.name + '.js'))
-            .pipe(uglify(options.uglify))
-            .pipe(header(fs.readFileSync('./banners/webextension.txt', 'utf8'), {
-                'package': package
-            }))
-        )
-        .pipe(gulp.dest('./dist/webextension/scripts'));
-});
-gulp.task('build:webextension:manifest', function () {
-    return gulp.src(['./manifest.json'])
-        .pipe(ejs({
-            'package': package
-        }))
-        .pipe(gulp.dest('./dist/webextension'));
-});
-gulp.task('build:webextension:locales', function () {
-    return gulp.src(['./_locales/**/*.json'])
-        .pipe(gulp.dest('./dist/webextension/_locales'));
-});
-gulp.task('build:webextension:logo', function () {
-    return gulp.src(['./resources/logo.svg'])
-        .pipe(gulp.dest('./dist/webextension'));
-});
-gulp.task('build:webextension', function (callback) {
-    sequence(
-        ['build:webextension:scripts', 'build:webextension:logo', 'build:webextension:locales', 'build:webextension:manifest'], ['zip:webextension', 'crx:webextension'],
-        callback
-    );
-});
 
-gulp.task('build:vendor', function () {
-    return gulp.src(vendor)
-        .pipe(gulp.dest('./dist/vendor'));
-});
+// ========================
+// package/distribute tasks
+// ========================
 
-// tasks for packaging the WebExtension for distribution
-gulp.task('zip:webextension', function () {
+gulp.task('zip', function (callback) {
     return gulp.src(['./dist/webextension/**/*', '!Thumbs.db'])
         .pipe(zip(package.name + '.zip'))
         .pipe(gulp.dest('./dist'));
 });
-gulp.task('crx:webextension', function () {
-    return gulp.src(['./dist/webextension', '!Thumbs.db'])
+gulp.task('crx', function () {
+    return gulp.src(['./dist/webextension/chrome', '!Thumbs.db'])
         .pipe(crx({
             'privateKey': fs.readFileSync('./certs/' + package.name + '.pem', 'utf8'),
             'filename': package.name + '.crx'
@@ -159,29 +150,36 @@ gulp.task('crx:webextension', function () {
         .pipe(gulp.dest('./dist'));
 });
 
-// task to rebuild everything on changes to core files
+
+// =========================
+// primary development tasks
+// =========================
+
+gulp.task('lint', function (callback) {
+    sequence(
+        ['lint:components', 'lint:helpers', 'lint:pages', 'lint:scripts'],
+        callback
+    );
+});
+
+gulp.task('build', ['clean'], function (callback) {
+    sequence(
+        ['build:logo', 'build:locales', 'build:manifest'],
+        ['build:includes', 'build:scripts'],
+        ['build:userscript'],
+        ['zip', 'crx'],
+        callback
+    );
+});
+
 gulp.task('watch', ['build'], function () {
-    options.uglify.drop_console = false;
-    return gulp.watch(path.join(_folders.scripts, '**/*.*'), ['build']);
+    options.uglify.compress.drop_console = false;
+
+    gulp.watch(includes, ['build:includes']);
+    gulp.watch(path.join(_folders.locales, '/**/*.json'), ['build:locales']);
+    gulp.watch('./manifest.json', ['build:manifest']);
+    gulp.watch(path.join(_folders.scripts, '/**/*.js'), ['build:scripts', 'build:userscript']);
 });
 
-// task to rebuild the userscript version on changes to core files
-gulp.task('watch:userscript', ['build:userscript'], function () {
-    return gulp.watch(path.join(_folders.scripts, '**/*.*'), ['build:userscript']);
-});
-
-// task to rebuild the WebExtension version on changes to core files
-gulp.task('watch:webextension', ['build:webextension'], function () {
-    return gulp.watch(path.join(_folders.scripts, '**/*.*'), ['build:webextension']);
-});
-
-// task for cleaning everything
-gulp.task('clean', ['clean:userscript', 'clean:webextension']);
-
-// task for building everything
-gulp.task('build', ['build:userscript', 'build:webextension']);
-
-// default task: cleans and builds everything
-gulp.task('default', function (callback) {
-    sequence('clean', 'build', callback);
-});
+// default task (alias build)
+gulp.task('default', ['build']);
