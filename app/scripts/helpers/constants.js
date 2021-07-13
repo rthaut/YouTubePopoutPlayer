7ed668de14cb2354e8da6bcbd1ecdb716136aa14
Ryan Thaut <rthaut@gmail.com>
2021-07-12 21:59:25 -0600

    lint changes for constants file

--- app/scripts/helpers/constants.js
+++ app/scripts/helpers/constants.js
@@ -27,6 +27,10 @@ export const OPTION_DEFAULTS = {
 };
 
 export const OPTIONS_BEHAVIOR_TARGET_VALUES = ["window", "tab"];
-export const OPTIONS_BEHAVIOR_CONTROLS_VALUES = ["none", "standard", "extended"];
+export const OPTIONS_BEHAVIOR_CONTROLS_VALUES = [
+  "none",
+  "standard",
+  "extended",
+];
 export const OPTIONS_SIZE_MODE_VALUES = ["current", "custom"];
 export const OPTIONS_SIZE_UNITS_VALUES = ["pixels", "percentage"];
