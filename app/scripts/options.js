df4f4de4d0caf8a717341cbe63a0070dd61d59e5
Ryan Thaut <rthaut@gmail.com>
2021-07-22 22:00:00 -0600

    adjust width of Options page to only apply in Chrome

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -14,3 +14,9 @@ import ReactDOM from "react-dom";
 import OptionsApp from "./options/OptionsApp";
 
 ReactDOM.render(<OptionsApp />, document.querySelector("#root"));
+
+const params = new URL(document.location).searchParams;
+const vendor = params.get("vendor");
+if (vendor) {
+  document.documentElement.classList.add(`browser-${vendor}`);
+}
