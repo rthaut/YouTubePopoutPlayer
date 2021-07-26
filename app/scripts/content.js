6d17001e9f9627e34357ea0d8a27a8bb335d5fff
Ryan Thaut <rthaut@gmail.com>
2021-07-26 11:47:42 -0600

    fix popout player close event handler not firing in some browsers

--- app/scripts/content.js
+++ app/scripts/content.js
@@ -6,10 +6,10 @@ import { InsertControlsAndWatch } from "./content/YouTubeCustomControls";
 import { IsPopoutPlayer, debounce } from "./helpers/utils";
 
 /**
- * Registers event listeners for when the window is resized and closed.
+ * Registers event listeners for when the Popout Player window is resized and closed.
  * This is done in the content script, as there is no way to get window size or position data from the background script.
  */
-const RegisterWindowEventListeners = () => {
+const RegisterEventListeners = () => {
   // window resize event listener (debounced for performance)
   window.addEventListener(
     "resize",
@@ -20,11 +20,11 @@ const RegisterWindowEventListeners = () => {
 
   // alternative to window close event listener (as recommended by MDN),
   // used for situations where the user moves the window AFTER resizing it,
-  // as there is no native move / reposition event
-  // TODO: this event/message doesn't fire occasionally in Chrome (and it never fires in Edge)
-  window.addEventListener("unload", () => {
+  // as there is no native move/(re)position event
+  // alternative syntax for `beforeunload` event registration, seems to have better browser support
+  window.onbeforeunload = () => {
     SendWindowDimensionsAndPosition("popout-closed");
-  });
+  };
 };
 
 const SendWindowDimensionsAndPosition = async (action) => {
@@ -67,6 +67,6 @@ const SendWindowDimensionsAndPosition = async (action) => {
   browser.runtime.onMessage.addListener(OnRuntimeMessage);
 
   if (IsPopoutPlayer(window.location)) {
-    RegisterWindowEventListeners();
+    RegisterEventListeners();
   }
 })();
