3fb2d97bac92034db80d0f026bdc88e1536fd4e3
Ryan Thaut <rthaut@gmail.com>
2024-10-15 09:37:48 -0600

    migrate to `declarativeNetRequest` for manifest v3 (used to ensure the `Referer` header is set on embedded player in the popout player)

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -2,12 +2,6 @@ import { OnCommandEventHandler } from "./background/commands";
 import { InitMenus } from "./background/menus";
 import { OpenPopoutBackgroundHelper } from "./background/popout";
 import { OnInstalled, OnRuntimeMessage } from "./background/runtime";
-import {
-  GetExtraInfoSpec,
-  GetFilter,
-  OnBeforeSendHeaders,
-  OnSendHeaders,
-} from "./background/webRequest";
 import Options from "./helpers/options";
 import { IsVideoURL } from "./helpers/youtube";
 
@@ -28,18 +22,6 @@ browser.commands.onCommand.addListener(OnCommandEventHandler);
 browser.runtime.onInstalled.addListener(OnInstalled);
 
 browser.runtime.onMessage.addListener(OnRuntimeMessage);
-
-browser.webRequest.onBeforeSendHeaders.addListener(
-  OnBeforeSendHeaders,
-  GetFilter("onBeforeSendHeaders"),
-  GetExtraInfoSpec("onBeforeSendHeaders"),
-);
-
-browser.webRequest.onSendHeaders.addListener(
-  OnSendHeaders,
-  GetFilter("onSendHeaders"),
-  GetExtraInfoSpec("onSendHeaders"),
-);
 
 browser.tabs.onUpdated.addListener(async (tabId, changeInfo) => {
   if (changeInfo.url && IsVideoURL(changeInfo.url, false)) {
--- webextension-toolbox.config.js
+++ webextension-toolbox.config.js
@@ -1,9 +1,20 @@
 /* global require */
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const webpack = require("webpack");
 
 /* global module */
 module.exports = {
-  webpack: (config, { vendor }) => {
+  webpack: (config, { dev, vendor }) => {
+    // copy the static declarativeNetRequest rule files
+    config.plugins.push(
+      new CopyWebpackPlugin({
+        patterns: [{
+          from: 'rules*.json',
+          to: config.target
+        }]
+      })
+    );
+
     // TODO: it would be nice to drop the polyfill, but it seems chrome still only recognizes the `chrome` namespace...
     if (["chrome", "opera", "edge"].includes(vendor)) {
       config.plugins.push(
