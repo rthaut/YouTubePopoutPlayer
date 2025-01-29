1859d50bcca9b5a438305606327142d49a0a2024
Ryan Thaut <rthaut@gmail.com>
2025-01-29 15:47:52 -0700

    rename and move icons for wxt to auto-include them in manifest

--- scripts/generate-icons.js
+++ scripts/generate-icons.js
@@ -1,13 +1,14 @@
 const path = require("path");
-const sharp = require("sharp")
+const sharp = require("sharp");
 
 const sizes = [16, 19, 24, 32, 38, 48, 64, 96, 128];
 const inputFile = path.resolve("resources/logo.svg");
-const outputDir = path.resolve("app/images/");
+const outputDir = path.resolve("src/public/icons");
 
-sizes.forEach((size) => sharp(inputFile)
-  .resize(size, size, { fit: "contain", background: "transparent" })
-  .png()
-  .toFile(path.join(outputDir, `icon-${size}.png`))
-  .then(() => console.log("Created", `icon-${size}.png`, "in", outputDir))
+sizes.forEach((size) =>
+  sharp(inputFile)
+    .resize(size, size, { fit: "contain", background: "transparent" })
+    .png()
+    .toFile(path.join(outputDir, `${size}.png`))
+    .then(() => console.log("Created", `${size}.png`, "in", outputDir)),
 );
