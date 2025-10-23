2a551ac5405f1748e9a9043800b11a547fa91ce3
Ryan Thaut <rthaut@gmail.com>
2025-10-23 13:49:18 -0600

    fix options panel width (chrome) and horizontal overscrolling (firefox)

--- src/entrypoints/options/main.tsx
+++ src/entrypoints/options/main.tsx
@@ -6,9 +6,3 @@ import OptionsApp from "./OptionsApp";
 const container = document.querySelector("#root")!;
 const root = createRoot(container);
 root.render(<OptionsApp />);
-
-const params = new URL(window.location.href).searchParams;
-const vendor = params.get("vendor");
-if (vendor) {
-  document.documentElement.classList.add(`browser-${vendor}`);
-}
